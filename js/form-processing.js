1905d6122bde5ac479e1b4e5e78baed4ea2de8fa
ddrieck <ddrieck@gmail.com>
2015-01-01 12:35:23 -0800

    Trying other method to upload form data.

--- js/form-processing.js
+++ js/form-processing.js
@@ -7,27 +7,29 @@ $(document).ready(function(){
 		$(".party-div, .guests-div").hide();
 	});
 
+	var googleForm = $("#input-form").jqGoogleForms({"formKey": "1Iza9GWuT8LU0AY4kzv2e039LxlL7R3FBdPdzmI7aYHs"});
+
 	$('#input-form').submit(function(){
 		event.preventDefault();
-		var inputName = 'entry.991411239=' + encodeURIComponent($('#name').val()) + "&";
-		var inputEmail = 'entry.797029182=' + encodeURIComponent($('#email').val()) + "&";
-		var inputYes = 'entry.37903139=' + encodeURIComponent($('#yes-select').val()) + "&";
-		var inputNo = 'entry.37903139=' + encodeURIComponent($('#no-select').val()) + "&";
-		var inputParty = 'entry.1901209542=' + encodeURIComponent($('#party').val()) + "&";
-		var inputGuests = 'entry.1852347906='+ encodeURIComponent($('#guests').val()) + "&";
-		var inputComments = 'entry.390843563=' + encodeURIComponent($('#comments').val()) + "&";
 
-		var baseURL = 'https://docs.google.com/forms/d/1Iza9GWuT8LU0AY4kzv2e039LxlL7R3FBdPdzmI7aYHs/formResponse?';
-		var submitRef = '&submit=Submit';
-		var submitURL = (baseURL + inputName + inputEmail + inputYes + inputNo + inputParty + inputGuests + inputComments + submitRef);
-		console.log(submitURL);
+		var inputName = encodeURIComponent($('#name').val());
+		var inputEmail = encodeURIComponent($('#email').val());
+		var inputYes = encodeURIComponent($('#yes-select').val());
+		var inputNo = encodeURIComponent($('#no-select').val());
+		var inputParty = encodeURIComponent($('#party').val());
+		var inputGuests = encodeURIComponent($('#guests').val());
+		var inputComments = encodeURIComponent($('#comments').val());
 
-		 var posting = $.post( submitURL);
-		// Put the results in a div
-		posting.done(function( data ) {
-		var content = $( data ).find( "#content" );
-		$( "#result" ).empty().append( content );
-		
+		googleForm.sendFormData({
+    		"entry.991411239": inputName,
+    		"entry.797029182": inputEmail,
+    		"entry.37903139" : inputYes,
+    		"entry.37903139" : inputNo,
+    		"entry.1901209542" : inputParty,
+    		"entry.1852347906" : inputGuests,
+    		"entry.390843563" : inputComments
 		});
+
+
 	});
 });
